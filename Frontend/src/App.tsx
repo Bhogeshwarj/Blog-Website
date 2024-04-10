67156f6b5873846e99d404d232930025e7f65302
Bhogeshwar Jadhav <bhogeshwarj@gmail.com>
2024-04-10 21:56:41 +0530

    Removed a bug

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Routes>
+        <Routes >
           <Route path="/" element={<Home />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,17 +1,17 @@
-
 import { Link } from "react-router-dom"
-import Signup from "./Signup"
+import Signin from "./Signin"
 
 const Home = () => {
   return (
     <div>
 <div className="border-b flex justify-between px-10 py-4">
+<div>
         <Link to={'/'} className="flex flex-col justify-center cursor-pointer text-2xl font-bold">
                 Blog
         </Link>
         </div>
-<Signup />
-
+        <Signin />
+    </div>
     </div>
   )
 }
