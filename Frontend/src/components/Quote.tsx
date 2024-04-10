e5efdc5a5ada138c7bbd7b55d81eba48ef773a5d
Bhogeshwar Jadhav <bhogeshwarj@gmail.com>
2024-04-10 15:46:15 +0530

    Setup fronted and added some pages

---
+++ Frontend/src/components/Quote.tsx
@@ -0,0 +1,11 @@
+
+
+const Quote = () => {
+  return (
+    <div className="bg-slate-200 h-screen ">
+        Quote Component
+    </div>
+  )
+}
+
+export default Quote
---
+++ Frontend/src/pages/Signup.tsx
@@ -0,0 +1,12 @@
+import Quote from "../components/Quote"
+
+
+const Signup = () => {
+  return (
+    <div>Signup
+        <Quote />
+    </div>
+  )
+}
+
+export default Signup
